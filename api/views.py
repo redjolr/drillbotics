81a6eb039db2588f35361dd0f91553cecc12d0a3
Redjol Resulaj <redjol.resulaj@yahoo.com>
2019-02-22 18:49:19 +0100

    Before celery

--- api/views.py
+++ api/views.py
@@ -54,48 +54,31 @@ def addexperiment(request, checksum):
         print("Adding experiment yoo", checksum)
         with open(meta_file, "r") as f:
             experiment_meta = json.load(f)
-
-
         experiment_start_unix =   int(time.mktime(time.strptime(experiment_meta['start_time'], '%Y-%m-%d %H:%M:%S')))*(10**6)
-
-
         rock = Rock.objects.get(id=experiment_meta['rock_id'])
-
         experiment = Experiment(start_time = experiment_meta['start_time'], description = experiment_meta['description'], rock_id=rock, checksum=checksum )
         experiment.save()
-
-
-
         sql = '''INSERT INTO measurement(time_micro, value, depth, experiment_id, sensor_id) VALUES(%s, %s, %s, %s, %s)'''
-
-
-
-
-
-
-
-
-
-
-
-        # df_chunks = [pd.DataFrame(df[i:i+chunk_size]) for i in range(0, len(df), chunk_size) ]
 
         t1 = time.time()
         chunk_ind=0
         chunk_size = 50
         for df_chunk in pd.read_csv(dump_file, chunksize=chunk_size):
-            print("Started chunk ", chunk_ind)
-            df_chunk["time_micro"] = [int(x*(10**6))+experiment_start_unix for x in df_chunk["time"] ]
-            sensors_abbrs = list(df_chunk.columns.values)
-            sensors = { sensor['abbreviation']:sensor['id'] for sensor in list(Sensor.objects.filter(abbreviation__in=sensors_abbrs).values('abbreviation', 'id')) }
-            bulk_measurements = []
-            for sensor, sensor_id in sensors.items():
-                for i in range(len(df_chunk)):
-                    bulk_measurements.append( ( int(df_chunk['time_micro'][i+chunk_ind*chunk_size]), df_chunk[sensor][i+chunk_ind*chunk_size], 0, experiment.id, sensor_id) )
+            try:
+                print("Started chunk ", chunk_ind)
+                df_chunk["time_micro"] = [int(x*(10**6))+experiment_start_unix for x in df_chunk["time"] ]
+                sensors_abbrs = list(df_chunk.columns.values)
+                sensors = { sensor['abbreviation']:sensor['id'] for sensor in list(Sensor.objects.filter(abbreviation__in=sensors_abbrs).values('abbreviation', 'id')) }
+                bulk_measurements = []
+                for sensor, sensor_id in sensors.items():
+                    for i in range(len(df_chunk)):
+                        bulk_measurements.append( ( int(df_chunk['time_micro'][i+chunk_ind*chunk_size]), df_chunk[sensor][i+chunk_ind*chunk_size], 0, experiment.id, sensor_id) )
 
-            with connection.cursor() as cursor:
-                cursor.executemany(sql,bulk_measurements)
-            chunk_ind+=1
+                with connection.cursor() as cursor:
+                    cursor.executemany(sql,bulk_measurements)
+                chunk_ind+=1
+            except Exception as err:
+                print("ERROR BRO: ", str(err))
 
 
 
