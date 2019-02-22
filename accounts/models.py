b2f610e81d0c13d6bcd41b7592a72c2b279df184
Redjol Resulaj <redjol.resulaj@yahoo.com>
2019-02-22 12:18:45 +0100

    user change

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Occupation(models.Model):
     name = models.CharField(max_length=50)
